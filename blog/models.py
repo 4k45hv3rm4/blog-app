f7f3fb1be84d0d56af338d3f65baa34716161da3
4k45hv3rm4 <4k45hr0ck5007@gmail.com>
2020-01-22 18:04:37 +0530

    added Post and Comment Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,59 @@
-from django.db import models
+#FEATURES:
+    #Create/Edit blog post(admin)
+    #View blog post(public)
+    # Comment on a blog post(anonymous)
+    # Store anonymoues user details in session
+    # Show month based blog archives
+    # Generate RSS Feed
 
-# Create your models here.
+##MODELS USED
+    #POST
+        #title
+        # Text content
+        # slug
+        # Created date
+        # Author
+    #COMMENT
+        #name
+        #website
+        #Email
+        #text Content
+        #Post Related
+        # Created Date
+
+from django.db import models
+from django.template.defaultfilters import slugify
+
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    slug=models.SlugField(unique=True)
+    text=models.TextField()
+    created_on=models.DateTimeField(auto_now_add=True)
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        retunr ('blog_post_detail',(),
+                {
+                    'slug':self.slug,
+                })
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug=slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+class Comment(models.Model):
+    name=models.CharField(max_length=42)
+    email=models.EmailField(max_length=75)
+    website=models.URLField(max_length=200, null=True)
+    text=models.TextField()
+    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.text
+
